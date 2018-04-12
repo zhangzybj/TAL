015dc0f2b78b5b6798cfc226c6088c823daf1c64
zzy <758245446@qq.com>
2018-04-12 11:33:35 +0800

    web 项目 修改

--- src/main/java/com/tal/xes/Application.java
+++ src/main/java/com/tal/xes/Application.java
@@ -3,6 +3,8 @@ package com.tal.xes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.tal.xes.mapper")
@@ -11,4 +13,17 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
 }
+//public class Application extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Application.class);
+//    }
+//
+//    public static void main(String[] args) throws Exception {
+//        SpringApplication.run(Application.class, args);
+//    }
+//
+//}
